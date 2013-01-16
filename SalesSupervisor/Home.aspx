c3af245cf164b309a03b296132be21c3c54e605e
Ubaid Khan <ubaidkhan88@gmail.com>
2013-01-16 12:36:15 +0300

    updated sales supervisor home page

--- SalesSupervisor/Home.aspx
+++ SalesSupervisor/Home.aspx
@@ -4,5 +4,12 @@
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" Runat="Server">
 <h3>Home Page For Sales Supervisor !</h3>
+<hr />
+<h3 class="left">My Sales Users</h3>
+<div class="clear"></div>
+    <asp:GridView ID="gvSalesUsers" runat="server" AllowPaging="True" 
+        onpageindexchanging="gvSalesUsers_PageIndexChanging" 
+        onrowdatabound="gvSalesUsers_RowDataBound" Width="100%">
+    </asp:GridView>
 </asp:Content>
 
--- SalesSupervisor/Home.aspx.cs
+++ SalesSupervisor/Home.aspx.cs
@@ -4,11 +4,55 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using QuotationTrackingSystemDBModel;
+using System.Data;
 
 public partial class SalesSupervisor_Home : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        BindDataToGridView();
+    }
 
+     protected void gvSalesUsers_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        gvSalesUsers.PageIndex = e.NewPageIndex;
+        BindDataToGridView();
+    }
+    protected void gvSalesUsers_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+        if (e.Row.RowType != DataControlRowType.DataRow) return;
+        for (int i = 0; i < e.Row.Cells.Count; i++)
+        {
+            e.Row.Cells[i].HorizontalAlign = HorizontalAlign.Center;
+        }
+    }
+
+    protected void BindDataToGridView()
+    {
+        QuotationTrackingSystemDBEntities _quotationTrackingSystemDBEntities = new QuotationTrackingSystemDBEntities();
+        DataTable dt = new DataTable();
+        DataRow dr = null;
+
+        dt.Columns.Add(new DataColumn("User Name", typeof(string)));
+        dt.Columns.Add(new DataColumn("Role", typeof(string)));
+        dt.Columns.Add(new DataColumn("Status", typeof(string)));
+        dt.Columns.Add(new DataColumn("Created At", typeof(string)));
+
+        int CurrentUserId = CurrentUser.Id();
+        var list = _quotationTrackingSystemDBEntities.tblUsers.Where(x => x.SupervisorId == CurrentUserId).ToList();
+
+        foreach (var x in list)
+        {
+            dr = dt.NewRow();
+            dr["User Name"] = x.UserName;
+            dr["Role"] = StringHelper.ToSentenceCase(x.Role);
+            dr["Status"] = x.Status;
+            dr["Created At"] = DateTimeHelper.ConvertToString(x.CreatedAt.ToString());
+            dt.Rows.Add(dr);
+        }
+
+        gvSalesUsers.DataSource = dt;
+        gvSalesUsers.DataBind();
     }
 }
